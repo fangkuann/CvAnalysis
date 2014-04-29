bbaf20a5047d5bc165a9f809eaec97543f3024a1
fangkuann <fangkuann@gmail.com>
2014-04-29 17:43:23 +0800

    job skill pairs

--- columns.py
+++ columns.py
@@ -3,16 +3,16 @@ import jieba
 
 jskill = {}
 fin = open('d:/yincai_record_30w.txt', 'r')
-# for line in fin:
-#     cv = line.strip().replace('null', "'null'")
-#     if cv.find('php') != -1:
-#         cv = eval(cv)
-#         if cv['last_refresh'] != 'null':
-#            #print len(cv.keys()),'\t',cv['experience'][0],'\t',cv['projects'][0],'\t',cv['work_status'],'\t',cv['pro_title'][0]#,'\t',cv['f_lang'] ,
-#             try:
-#                 print cv['experience'][0]['work_desc']#,'\t',cv['experience'][0]['job_name'],'\t',cv['experience'][0]['com_name'],'\t',cv['experience'][0]['com_type']
-#             except TypeError:
-#                 continue
-#         continue
-#     # print '\n'.join(cv.keys())
-#     # break
+for line in fin:
+    cv = line.strip().replace('null', "'null'")
+    if cv.find('php') != -1:
+        cv = eval(cv)
+        if cv['last_refresh'] != 'null':
+           #print len(cv.keys()),'\t',cv['experience'][0],'\t',cv['projects'][0],'\t',cv['work_status'],'\t',cv['pro_title'][0]#,'\t',cv['f_lang'] ,
+            try:
+                print cv['experience'][0]['work_desc']#,'\t',cv['experience'][0]['job_name'],'\t',cv['experience'][0]['com_name'],'\t',cv['experience'][0]['com_type']
+            except TypeError:
+                continue
+        continue
+    # print '\n'.join(cv.keys())
+    # break
--- jobCounter.py
+++ jobCounter.py
@@ -1,17 +1,38 @@
+# coding=utf-8
 __author__ = 'fangkuan'
 import collections
 import jieba
 
 jskill = {}
-fin = open('d:/yincai_record_30w.txt', 'r')
+count = 0
+fin = open(u'F:/人才雷达/yincai_record_30w.txt', 'r')
 for line in fin:
+    count += 1
+    # if count == 1000:
+    #     break
     cv = line.strip().replace('null', "'null'")
     cv = eval(cv)
     skill = cv['t_pre_skill']
     job = cv['pro_title']
+    if job != 'null' and len(job) > 0 and skill != 'null':
+        job = job[0]
+    else:
+        continue
     if job not in jskill:
         jskill.update({job: [skill]})
     else:
         jskill[job].append(skill)
+
 fin.close()
 
+fout = open('./job_skill.txt', 'w')
+for job in jskill:
+    c = collections.Counter(jskill[job])
+    common = c.most_common(10)
+    jobs = []
+    for j in common:
+        jobs.append(j[0])
+
+    fout.write(job+'\t'+','.join(jobs)+'\n')
+fout.flush()
+fout.close()
